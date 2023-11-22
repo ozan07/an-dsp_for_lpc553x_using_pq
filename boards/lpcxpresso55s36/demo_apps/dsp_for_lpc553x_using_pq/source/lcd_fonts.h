6a78006d3fcdf32407c23c70166f18e6975a8fc8
yandld <1453363089@qq.com>
2023-11-22 20:27:45 +0800

    add font
    add font

---
+++ boards/lpcxpresso55s36/demo_apps/dsp_for_lpc553x_using_pq/source/lcd_fonts.h
@@ -0,0 +1,30 @@
+/*
+* Copyright 2023 NXP
+* All rights reserved.
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _USE_FONTS_H
+#define _USE_FONTS_H
+
+
+/* Includes ------------------------------------------------------------------*/
+//#include "MacroAndConst.h"
+
+#include "stdint.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+extern const uint8_t c_chFont1206[95][12];
+extern const uint8_t c_chFont1608[95][16];
+
+#endif
+
+/*-------------------------------END OF FILE-------------------------------*/
+
